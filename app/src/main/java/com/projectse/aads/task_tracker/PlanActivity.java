dc90e58800ad8f15b7487e60a8176ac4fe942cb5
Davlatbek Isroilov <idavlatbek19@gmail.com>
2016-02-08 00:26:18 +0300

    Added addtask func in plan activity

---
+++ app/src/main/java/com/projectse/aads/task_tracker/PlanActivity.java
@@ -0,0 +1,140 @@
+package com.projectse.aads.task_tracker;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.projectse.aads.task_tracker.DBService.DatabaseHelper;
+import com.projectse.aads.task_tracker.Models.TaskModel;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Shows list of tasks
+ */
+public class PlanActivity extends AppCompatActivity {
+
+    ArrayList<TaskModel> taskList = new ArrayList<>();
+
+    StableArrayAdapter adapter = null;
+    ListView listview = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        final DatabaseHelper db = DatabaseHelper.getsInstance(this);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plan);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        final ListView listview = (ListView) findViewById(R.id.listview);
+
+        List<TaskModel> list = db.getTaskModelList();
+
+        adapter = new StableArrayAdapter(this,
+                android.R.layout.simple_list_item_1, list);
+				
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                final TaskModel item = (TaskModel) parent.getItemAtPosition(position);
+                callEditTaskActivity(item);
+           }
+
+       });
+    }
+
+    /**
+     * sub class for taking list item
+     */
+    private class StableArrayAdapter extends ArrayAdapter<TaskModel> {
+
+        HashMap<TaskModel, Integer> mIdMap = new HashMap<TaskModel, Integer>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  List<TaskModel> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            TaskModel item = getItem(position);
+            return mIdMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_plan, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * sending task object and starting Edit Task activity
+     * @param task
+     */
+    public void callEditTaskActivity(TaskModel task){
+        Intent intent = new Intent (getApplicationContext(), TaskEditActivity.class);
+        intent.putExtra("task_id", task.getId());
+//        onPause();
+        startActivityForResult(intent,0);
+        adapter.notifyDataSetChanged();
+//        onResume();
+    }
+
+    public void callAddTaskActivity(){
+        Intent intent = new Intent (getApplicationContext(), AddTaskActivity.class);
+        startActivity(intent);
+    }
+}

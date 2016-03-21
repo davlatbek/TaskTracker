868b307b47df59b62bceaaa91049bf9c603211c9
Anastasia Puzankova <Anastasia Puzankova>
2016-03-21 06:50:44 +0300

    Little changes

--- app/src/main/java/com/projectse/aads/task_tracker/CoursePlanActivity.java
+++ app/src/main/java/com/projectse/aads/task_tracker/CoursePlanActivity.java
@@ -7,37 +7,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.AdapterView;
+
 
 import com.projectse.aads.task_tracker.DBService.DatabaseHelper;
 import com.projectse.aads.task_tracker.Models.TaskModel;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Anastasia A. Puzankova on 25-Feb-16.
  */
-public class CourseListActivity extends AppCompatActivity {
+public class CoursePlanActivity extends AppCompatActivity {
     ArrayList<TaskModel> taskList = new ArrayList<>();
-    StableArrayAdapter adapter = null;
+    //StableArrayAdapter adapter = null;
     ListView listview = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-
         DatabaseHelper db = DatabaseHelper.getsInstance(getApplicationContext());
+        setContentView(R.layout.course_plan);
 
-        setContentView(R.layout.activity_plan);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        //db.deleteTaskTable(db.getWritableDatabase());
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                callAddTaskActivity();
-            }
-        });
+        List<TaskModel> list = db.getListOfTasks(1);
+        ListView listview = (ListView) findViewById(R.id.list_of_tasks);
+        taskList = (ArrayList<TaskModel>) db.getTaskModelList();
+     //      adapter = new StableArrayAdapter(this,
+     //             android.R.layout.simple_list_item_1, taskList);
+
+     //     listview.setAdapter(adapter);
     }
 }

c728bafbad60b7d432fa285bee013ff6cf15918b
Anastasia Puzankova <Anastasia Puzankova>
2016-03-25 20:11:12 +0300

    add course_list.xml

--- app/src/main/java/com/projectse/aads/task_tracker/Fragments/CoursesFragment.java
+++ app/src/main/java/com/projectse/aads/task_tracker/Fragments/CoursesFragment.java
@@ -1,10 +1,13 @@
 package com.projectse.aads.task_tracker.Fragments;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.projectse.aads.task_tracker.R;
 
 
 /**
@@ -14,6 +17,8 @@ public class CoursesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         getActivity().setTitle("Courses");
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_course_list, container, false);
+
+        return view;
     }
 }

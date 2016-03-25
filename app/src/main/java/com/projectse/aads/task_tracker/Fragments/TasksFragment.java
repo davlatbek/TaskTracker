de834ec15bf0b872ae089ac040665e5522343efa
Sergey Kuznetsov <s.smith@bk.ru>
2016-03-25 09:46:04 +0300

    Fix, Tasks Fragment. Icons on the center. Warn! Buttons are layout now, manualy create onClick() for RelativeLayout.

--- app/src/main/java/com/projectse/aads/task_tracker/Fragments/TasksFragment.java
+++ app/src/main/java/com/projectse/aads/task_tracker/Fragments/TasksFragment.java
@@ -2,9 +2,12 @@ package com.projectse.aads.task_tracker.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.projectse.aads.task_tracker.R;
 
@@ -16,7 +19,12 @@ public class TasksFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         getActivity().setTitle("Tasks");
         View view = inflater.inflate(R.layout.fragment_tasks, container, false);
-//        return super.onCreateView(inflater, container, savedInstanceState);
+
+        // mapping correction
+        LinearLayout buttons_layout = (LinearLayout) view.findViewById(R.id.tasks_categories_buttons);
+        int width = getContext().getResources().getDisplayMetrics().widthPixels;
+        buttons_layout.setLayoutParams(new LinearLayout.LayoutParams(width,width));
+
         return view;
     }
 }

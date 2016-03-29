be4540b2db5558de3ece73d953fa13f221ecb885
Anastasia Puzankova <Anastasia Puzankova>
2016-03-29 16:19:36 +0300

    fix mergin

--- app/src/main/java/com/projectse/aads/task_tracker/Fragments/CoursesFragment.java
+++ app/src/main/java/com/projectse/aads/task_tracker/Fragments/CoursesFragment.java
@@ -27,7 +27,7 @@ import java.util.List;
 public class CoursesFragment extends Fragment {
 
     private onCourseClickListener courseClickEventListener;
-    private onCreateCpourseListener courseCreateClickListener;
+   // private onCreateCpourseListener courseCreateClickListener;
 
     public void setSomeEventListener(onCourseClickListener someEventListener) {
         this.courseClickEventListener = someEventListener;
@@ -121,49 +121,6 @@ public class CoursesFragment extends Fragment {
             addnewcourse.show();
         }
     };
-
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        getActivity().setTitle("Courses");
-        View view = inflater.inflate(R.layout.fragment_course_list, container, false);
-
-        ImageButton addRequestButton = (ImageButton) view.findViewById(R.id.create_request_fab);
-        addRequestButton.setOnClickListener(requestButtonListener);
-
-        DatabaseHelper db = DatabaseHelper.getsInstance(getActivity().getApplicationContext());
-
-        List<CourseModel> course_list = db.getCourseModelList();
-
-        for (CourseModel c : course_list) {
-
-            // get data from DB
-            View requestListItemView = inflater.inflate(R.layout.course_list_item, null);
-            TableLayout requestItemsTable = (TableLayout) view.findViewById(R.id.request_items_table);
-            TextView tv = (TextView) requestListItemView.findViewById(R.id.request_name);
-            TextView desc = (TextView) requestListItemView.findViewById(R.id.request_short_desc);
-            TextView id = (TextView) requestListItemView.findViewById(R.id.id_course);
-            tv.setText(c.getName());
-            TextView abrev = (TextView) requestListItemView.findViewById(R.id.abrevLbl);
-            abrev.setText(c.getAbbreviation());
-            abrev.setBackgroundResource(c.getClr());
-
-            desc.setText("www");
-            id.setText(c.getId().toString());
-            requestListItemView.setOnClickListener(requestItemListener);
-            requestItemsTable.addView(requestListItemView);
-        }
-
-        return view;
-    }
-
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        if (activity instanceof onCourseClickListener) {
-            courseClickEventListener = (onCourseClickListener) activity;
-        }
-    }
 
     public interface onCourseClickListener {
         public void callCourseOverviewFragment(long course_id);

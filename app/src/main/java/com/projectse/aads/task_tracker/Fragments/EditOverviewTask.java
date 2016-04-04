1212b5baca603b52ed064fd6469730b4328e22e0
Davlatbek Isroilov <idavlatbek19@gmail.com>
2016-04-04 20:26:09 +0300

    added EditOverviewTask fragment

---
+++ app/src/main/java/com/projectse/aads/task_tracker/Fragments/EditOverviewTask.java
@@ -0,0 +1,8 @@
+package com.projectse.aads.task_tracker.Fragments;
+
+/**
+ * Created by Davlatbek Isroilov on 4/4/2016.
+ * Innopolis University
+ */
+public class EditOverviewTask {
+}
---
+++ app/src/main/java/com/projectse/aads/task_tracker/Interfaces/TaskOverviewCaller.java
@@ -0,0 +1,8 @@
+package com.projectse.aads.task_tracker.Interfaces;
+
+/**
+ * Created by Davlatbek Isroilov on 4/4/2016.
+ * Innopolis University
+ */
+public interface TaskOverviewCaller {
+}
